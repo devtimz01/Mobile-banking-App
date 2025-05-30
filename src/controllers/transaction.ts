6f1a7b24e69f0ddb8ad3a006da6f6bcd29928078
devtimz01@gmail.com <moses1model@gmail.com>
2025-05-30 21:43:30 +0100

    deposit init

--- src/controllers/transaction.ts
+++ src/controllers/transaction.ts
@@ -9,10 +9,10 @@ class TransactionController{
     constructor(_transactionService:TransactionService){
         this.transactionService= _transactionService
     };
-   /* async initiateDeposit(req:Request,res:Response){
+     async initiateDeposit(req:Request,res:Response){
         try{
             const params= {...req.body}
-            const depositInfo= await PaymentService.paymentUrlLink(params.user.email, params.amount)
+            const depositInfo= await PaymentService.paymentUrlLink(req.user.email, params.amount)
              if(!depositInfo){
                 return res.status(500).send("depositURlLink error")}   
             const newTransaction={
@@ -29,37 +29,7 @@ class TransactionController{
             console.log(err)
             return res.status(500).send('deposit not initated')
         }      
-    };*/
-
-    async initiateDeposit(req: Request, res: Response) {
-  try {
-    const { amount, userId, accountId } = req.body;
-    const user = req.user; // <- pulled from Auth middleware
-
-    if (!user) {
-      return res.status(401).send("Unauthorized");
-    }
-
-    const depositInfo = await PaymentService.paymentUrlLink(user.email, amount);
-    if (!depositInfo) {
-      return res.status(500).send("depositURlLink error");
-    }
-
-    const newTransaction = {
-      userId,
-      accountId,
-      amount,
-      refId: depositInfo.reference,
-      details: {}
-    } as Itransaction;
-
-    const deposit = await this.transactionService.depositTransaction(newTransaction);
-    return res.status(201).json({ deposit });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send("Deposit not initiated");
-  }
-}
+    };
 
     async verifyTransaction(req:Request,res:Response){
         //get REFERENCE-LINK to verify transaction is successful
